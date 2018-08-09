764e5463ee5f87e308a66882fa86cbb205662c50
Fangluobin <q1217830193@163.com>
2018-08-09 14:23:55 +0800

    index finish

--- src/pages/home/components/Swiper.vue
+++ src/pages/home/components/Swiper.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="wrapper">
-    <swiper :options="swiperOption">
-      <swiper-slide v-for="item of swiperList" :key="item.id">
+    <swiper :options="swiperOption" v-if="showSwiper">
+      <swiper-slide v-for="item of list" :key="item.id">
         <img class="swiper-img" :src="item.imgUrl">
       </swiper-slide>
       <div class="swiper-pagination"  slot="pagination"></div>
@@ -12,19 +12,20 @@
 <script>
 export default {
   name: 'HomeSwiper',
+  props: {
+    list: Array
+  },
   data () {
     return {
       swiperOption: {
         pagination: '.swiper-pagination',
         loop: true
-      },
-      swiperList: [{
-        id: '0001',
-        imgUrl: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1533706605450&di=b6f9a79da85348023ed1d96e8358c540&imgtype=0&src=http%3A%2F%2Ffang.0579.cn%2FTopic%2F20150325%2Fimages%2Fcom_background_5.jpg'
-      }, {
-        id: '0002',
-        imgUrl: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1533708389656&di=e034ece7b2d8b0b908561bea49b6a940&imgtype=0&src=http%3A%2F%2Fimgqn.koudaitong.com%2Fupload_files%2F2014%2F08%2F31%2FFuFVyZvSzjjdz-UItbkOuMlX9iQ-.jpg'
-      }]
+      }
+    }
+  },
+  computed: {
+    showSwiper () {
+      return this.list.length
     }
   }
 }
